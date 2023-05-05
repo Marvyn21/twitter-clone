b0a9919325df55c7d65f0eae99bedb30a02e7244
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-05-05 13:15:56 +0300

    Feat: implement starter components

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,4 +3,32 @@ import { useHistory } from "react-router-dom";
 
 function Login() {
     const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        history.push('/feed');
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleLogin}>
+                <label>
+                    Username:
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </label>
+                <br />
+                <button type="submit" >Login</button>
+            </form>
+        </div>
+    );
 }
+
+export default Login;
